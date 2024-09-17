ea78be2cf5a708e0163e4662c575f7b2dbfdbd92
Tony Kan <tianchengkan@gmail.com>
2024-09-16 22:07:40 -0700

    feat(validation): Add display name validation utility
    - Implemented `validateDisplayName` function to ensure display names follow the required pattern.
      - No more than 20 characters.
      - No leading, trailing, or consecutive spaces.
    - Added corresponding regex pattern for display name validation.
    
    Related to issue #2

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -6,6 +6,7 @@
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // Check if the email is valid (contains an @ symbol and a period)
 const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_])[^\s]{8,}$/; // Check if the password is valid (at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character)
 const USERNAME_REGEX = /^[a-zA-Z0-9_]{3,}$/; // Check if the username is valid (at least 3 characters, alphanumeric characters and underscores only)
+const DISPLAY_NAME_REGEX = /^(?!.*\s{2,})[^\s](.{0,18}[^\s])?$|^$/; // Check if the display name is valid (no more than 20 characters, no leading or trailing spaces, no consecutive spaces)
 
 /**
  * @function validateEmail - Validate an email address.
@@ -56,6 +57,21 @@ const validateUsername = (username) => {
 };
 
 /**
+ * @function validateDisplayName - Validate a display name.
+ * @param {string} displayName - The display name to validate.
+ * @returns {boolean} - True if the display name is valid, false otherwise.
+ */
+const validateDisplayName = (displayName) => {
+  if (displayName === undefined || displayName === null) {
+    // Check if the display name is empty (null or undefined, but not an empty string)
+    return false;
+  } else {
+    // Check if the display name matches the regex pattern
+    return DISPLAY_NAME_REGEX.test(displayName);
+  }
+};
+
+/**
  * @function validateIdentifier - Validate an identifier. (email or username)
  * @param {string} identifier - The identifier to validate.
  * @returns {boolean} - True if the identifier is valid, false otherwise.
@@ -68,5 +84,6 @@ module.exports = {
   validateEmail,
   validatePassword,
   validateUsername,
+  validateDisplayName,
   validateIdentifier,
 };
