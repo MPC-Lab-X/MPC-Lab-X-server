66eebe37e98d39dd188543e01615629a9fa88495
Tony Kan <tianchengkan@gmail.com>
2024-09-21 17:28:19 -0700

    feat(utils): Add random utility functions for generating integers and variables
    - Implemented `randomInt` to generate a random integer within a specified range.
    - Added `randomVariable` to generate a random variable character (a-z or A-Z).
    
    Related to issue #4

---
+++ src/utils/randomUtils.js
@@ -0,0 +1,27 @@
+/**
+ * @file src/utils/randomUtils.js
+ * @description Random utility functions.
+ */
+
+/**
+ * @function randomInt - Generate a random integer between min and max (inclusive).
+ * @param {number} min - The minimum value (inclusive).
+ * @param {number} max - The maximum value (inclusive).
+ * @returns {number} - A random integer between min and max.
+ */
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+/**
+ * @function randomVariable - Generate a random variable. (a-z)
+ * @param {boolean} isUpperCase - Whether the variable should be uppercase. (A-Z)
+ * @returns {string} - A random variable.
+ */
+const randomVariable = (isUpperCase = false) => {
+  const min = isUpperCase ? 65 : 97;
+  const max = isUpperCase ? 90 : 122;
+  return String.fromCharCode(randomInt(min, max));
+};
+
+module.exports = { randomInt, randomVariable };
