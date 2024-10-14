404848ae0713d14eab22e83790055421847cc0da
Tony Kan <tianchengkan@gmail.com>
2024-10-14 09:10:22 -0700

    feat(middleware): Support IPv6 and mitigate ReDoS vulnerability
    - Enhanced the `preprocessRequestDetailsMiddleware` to support IPv6 addresses.
    - Implemented a length check for IP addresses (maximum length of 45 characters) to prevent Regular Expression Denial of Service (ReDoS) attacks.
    - Updated logic to ensure that IP address extraction from the `X-FORWARDED-FOR` header and request object is robust against excessively long inputs.
    - Default to "unknown" for cases where the IP address is invalid or not found.

--- src/middlewares/preprocessRequestDetailsMiddleware.js
+++ src/middlewares/preprocessRequestDetailsMiddleware.js
@@ -15,10 +15,24 @@ const preprocessRequestDetailsMiddleware = (req, res, next) => {
   if (req.headers["x-forwarded-for"]) {
     // Get the first IP address from the X-FORWARDED-FOR header
     const ip = req.headers["x-forwarded-for"].split(",")[0].trim();
-    req.ip = ip;
+
+    // Limit the length of the IP address to prevent ReDoS
+    if (ip.length <= 45) {
+      // Maximum length for IPv6 is 45 characters
+      req.ip = ip;
+    } else {
+      req.ip = "unknown";
+    }
   } else {
     // Populate the IP address from the request object
-    req.ip = (req.ip && req.ip.match(/\d+\.\d+\.\d+\.\d+/)?.[0]) || "unknown";
+    if (req.ip && req.ip.length <= 45) {
+      // Check length before matching
+      req.ip =
+        req.ip.match(/(?:\d{1,3}\.){3}\d{1,3}|[a-fA-F0-9:]+/g)?.[0] ||
+        "unknown";
+    } else {
+      req.ip = "unknown";
+    }
   }
 
   // Populate the User-Agent from the request object
